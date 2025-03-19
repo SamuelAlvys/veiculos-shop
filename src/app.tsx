cd9eed038e9675b934811c2f25f0acb588940654
Samuel Alves <samuel.alvyys@gmail.com>
2025-03-19 15:01:06 -0300

    init project

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+
+  return (
+    <div className="w-screen flex justify-center items-center items-cente">
+      <Button>
+        Hello, world!!!
+      </Button>
+    </div>
+  )
+}
